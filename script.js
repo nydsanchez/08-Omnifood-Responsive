1d049137d94879c9cde565239c616467b1df5fce
Nydia Massiel Sánchez <nydsanchez@gmail.com>
2024-03-23 18:10:12 -0600

    sticky menu

--- script.js
+++ script.js
@@ -13,6 +13,41 @@ btnNavEl.addEventListener("click", function () {
 });
 
 ///////////////////////////////////////////////////////////
+//SCROLL NAVIGATION//
+///////////////////////////////////////////////////////////
+const allLink = document.querySelectorAll("a:link");
+allLink.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    const href = link.getAttribute("href");
+
+    if (link.classList.contains("main-nav-link"))
+      headerEl.classList.toggle("nav-open");
+  });
+});
+
+///////////////////////////////////////////////////////////
+//STICKY MOBILE NAVIGATION//
+///////////////////////////////////////////////////////////
+
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    ent.isIntersecting
+      ? document.body.classList.remove("sticky")
+      : document.body.classList.add("sticky");
+  },
+  {
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+
+obs.observe(sectionHeroEl);
+
+///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
   var flex = document.createElement("div");
